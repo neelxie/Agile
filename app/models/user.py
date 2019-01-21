c11dc3099ac659cc7a348a42437e1c609987ded6
edna <edna@rr.com>
2019-01-21 11:44:58 +0300

    update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -2,19 +2,35 @@ users = []
 
 
 class User:
-    def __int__(self, username, password, role):
+    """class Users"""
+
+    def __init__(self, username, password, role):
         self.username = username
         self.password = password
         self.role = role
 
+    def user_to_dict(self):
+        """class Users"""
+
+        return{
+            "username": self.username,
+            "password": self.password,
+            "role": self.role
+        }
+
     def save_users(self, user):
         users.append(user)
 
+    def user_login(self, username, password):
+        for user in users:
+            if user["username"] == username and user["password"] == password:
+                return True
+            return False
+
     @staticmethod
     def search_user(username):
-    for user in users:
-        for key in user:
-            if user[key] == username:
+        for user in users:
+            if user["username"] == username:
                 return user
             else:
                 return username + ' was not found'
